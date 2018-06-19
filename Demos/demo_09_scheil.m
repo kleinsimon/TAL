fffff41755ebeb7b30a0db83ff12d3e76277f68a
Simon Klein <simon.klein@outlook.de>
2018-06-19 08:41:17 +0200

    rework

--- Demos/demo_09_scheil.m
+++ Demos/demo_09_scheil.m
@@ -1,11 +1,11 @@
-tcsys = sk_tc_system;
-tcsys.Database='TCFE9';
-tcsys.Elements='Fe c cr si v mn';
-tcsys.RejPhases='*';
-tcsys.ResPhases='fcc bcc cem liquid';
-tcsys.Init;
+TCSYS = sk_tc_system;
+TCSYS.Database='TCFE9';
+TCSYS.Elements='Fe c cr si v mn';
+TCSYS.RejPhases='*';
+TCSYS.ResPhases='fcc bcc cem liquid';
+TCSYS.Init;
 
-BaseEq=tcsys.GetInitialEquilibrium;
+BaseEq=TCSYS.GetInitialEquilibrium;
 BaseEq.SetCondition('t',       1500 + 273.15);
 BaseEq.SetCondition('p',       101325);
 BaseEq.SetCondition('n',       1);
@@ -14,7 +14,6 @@ BaseEq.SetCondition('w(cr)',   0.008);
 BaseEq.SetCondition('w(si)',   0.002);
 BaseEq.SetCondition('w(v)',    0.085);
 BaseEq.SetCondition('w(mn)',   0.003);
-
 Eq1 = BaseEq.Clone;
 
 Eq1.Calculate;
